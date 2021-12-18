1a0bc1d0860822b9e412da4f8184ad9831bbcf20
spr021 <saber.pourrahimi.1999@gmail.com>
2021-12-18 21:15:01 +0330

    remove extra file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as serviceWorker from "./serviceWorker";
 import NeshanMap from "./NeshanMap";
 
 ReactDOM.render(
@@ -33,8 +32,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
